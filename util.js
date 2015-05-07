2f96880a65aa23ab37a7a13e93c9aef049cbddd0
Hobart Sze <hobartsze@gmail.com>
2015-05-07 09:22:13 -0700

    remove timestamp

--- util.js
+++ util.js
@@ -1,6 +1,7 @@
 function makeUrl(m, a, c, k) {
   ts = Math.floor(Date.now()/1000) - 21;
-  url = "http://www.amazon.com/" + c + "/dp/" + a + "/?ie=UTF8&qid=" + ts;
+  url = "http://www.amazon.com/" + c + "/dp/" + a + "/?ie=UTF8"
+  // url = url + "&qid=" + ts;
   url = url + "&m=" + m + "&keywords=" + k;
   return url;
 }
